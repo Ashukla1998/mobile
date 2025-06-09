23ac1bbd69cc70f7f0079869555267334204029e
rahul <shuklarahul71@gmail.com>
2025-06-09 14:43:42 +0530

    reels

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,32 +12,32 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import posts from '../data/posts';
 import Post from '../components/Post';
 import Icon from 'react-native-vector-icons/Ionicons';
-
+import { FontAwesome } from '@expo/vector-icons';
 const stories = [
   {
     id: '1',
     name: 'Your Story',
-    image: { uri: 'https://source.unsplash.com/random/100x100?selfie' },
+    image: require('../assets/images/witcher.jpg'),
   },
   {
     id: '2',
     name: 'John',
-    image: { uri: 'https://source.unsplash.com/random/100x100?man' },
+    image: require('../assets/images/banner.jpg'),
   },
   {
     id: '3',
     name: 'Jane',
-    image: { uri: 'https://source.unsplash.com/random/100x100?woman' },
+    image: require('../assets/images/Eiffel_Tower_Vertical.jpg'),
   },
   {
     id: '4',
     name: 'Anna',
-    image: { uri: 'https://source.unsplash.com/random/100x100?girl' },
+    image: require('../assets/images/Eiffel_Tower_Vertical.jpg'),
   },
   {
     id: '5',
     name: 'Mike',
-    image: { uri: 'https://source.unsplash.com/random/100x100?boy' },
+    image: require('../assets/images/witcher.jpg'),
   },
 ];
 
@@ -48,10 +48,7 @@ export default function HomeScreen() {
 
       {/* Header */}
       <View style={styles.header}>
-        <Image
-          source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png' }}
-          style={styles.logo}
-        />
+        <FontAwesome name="instagram" size={40} color="#0000FF" />
         <View style={styles.headerIcons}>
           <Icon name="heart-outline" size={24} color="#000" style={styles.icon} />
           <Icon name="paper-plane-outline" size={24} color="#000" />
